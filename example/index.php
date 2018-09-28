2a3af31d70752d3ec2be7b35d3a4b4387f31b0ca
Josue Mazco Puma <JossMP@gmail.com>
2018-09-28 11:40:36 -0500

    Update 28/09/2018

--- example/index.php
+++ example/index.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
-	<head>
+  <head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name = "viewport" content = "initial-scale = 1.0, user-scalable = no,  width=device-width">
-		<title>Consulta RENIEC sin Captcha Perú - GeekDev</title>
-		<meta name="description" content="Consulta RENIEC sin Captcha Perú, descarga este codigo desde demos.geekdev.ml"/>
-		<meta name="keywords" content="buscar dni, consultar dni peru, api rest consulta dni peru, reniec perú, consulta dni sin captcha, dni reniec 2017"/>
+		<title>JossMP/datos-peru</title>
+		<meta name="description" content="Consulta datos de ciudadanos peruanos mediante su DNI"/>
 		<meta property="og:locale" content="es_PE" />
 		<meta property="og:type" content="website" />
-		<meta property="og:title" content="Consulta de DNI RENIEC sin Captcha Perú - GeekDev" />
-		<meta property="og:description" content="Consulta RENIEC sin Captcha Perú, descarga este codigo desde demos.geekdev.ml" />
-		<meta property="og:image" content="https://drive.google.com/uc?id=0BxTe_c1GIOkoSmtJMFZNRi1mNWs&export=view" />
+		<meta property="og:title" content="Consulta datos de ciudadanos peruanos mediante su DNI" />
+		<meta property="og:description" content="Consulta datos de ciudadanos peruanos con PHP sin captcha" />
+		<meta property="og:image" content="https://drive.google.com/uc?id=0BxTe_c1GIOkoaFpkZlNrR0tta0E&export=view" />
 		<!-- Bootstrap -->
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
@@ -27,23 +26,26 @@
 	</head>
 	<body>
 		<div class="container">
-			<h1 class="text-center p-3">Consulta DNI</h1>
+			<h1 class="text-center p-3"> Consulta Datos RENIEC </h1>
 			<form class="form-horizontal" role="form" method="post" name="busqueda" id="busqueda" >
-			<div class="card border-info">
-				<div class="card-header bg-info text-center text-light">
-					Ingrese los datos requeridos
+				<div class="card border-info">
+					<div class="card-header bg-info text-center text-light">
+						Ingrese los datos requeridos
+					</div>
+					<div class="card-body">
+						<input type="number" class="form-control" name="ndni" id="ndni" placeholder="Ingrese DNI" pattern="([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])" autofocus>
+					</div>
+					<div class="card-footer text-center">
+						<button type="submit" class="btn btn-success" name="btn-submit" data-source="EsSalud" id="btn-submit-1">
+							<i class="fa fa-search"></i> EsSalud
+						</button>
+						<button type="submit" class="btn btn-info" name="btn-submit" data-source="MinTra" id="btn-submit-2">
+							<i class="fa fa-search"></i> Ministerio del Trabajo
+						</button>
+					</div>
 				</div>
-				<div class="card-body">
-					<input type="number" class="form-control" name="ndni" id="ndni" placeholder="Ingrese DNI" pattern="([0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])" autofocus>
-				</div>
-				<div class="card-footer text-center">
-					<button type="submit" class="btn btn-success" name="btn-submit" id="btn-submit">
-						<i class="fa fa-search"></i> Buscar
-					</button>
-				</div>
-			</div>
 			</form>
-			
+
 			<div class="mx-auto text-center pt-2">
 				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 				<!-- ads_Content -->
@@ -55,8 +57,8 @@
 				(adsbygoogle = window.adsbygoogle || []).push({});
 				</script>
 			</div>
-			
-			<div class="result">
+
+			<div style="display:none" class="result">
 				<ul class="nav nav-tabs" role="tablist">
 					<li class="nav-item">
 						<a class="nav-link active" href="#home" data-toggle="tab" role="tab">Respuesta</a>
@@ -69,28 +71,10 @@
 					<div class="tab-pane active" id="home" role="tabpanel">
 						<div class="card border-info" id="home">
 							<div class="card-body">
-								<form class="form-horizontal row" name="FormData" id="FormData">
-									<div class="form-group col-10">
-										<label for="Nombre">DNI:</label>
-										<input type="text" class="form-control" id="DNI" placeholder="DNI">
-									</div>
-									<div class="form-group col-2">
-										<label for="CodVerificacion">Cod. Validacion:</label>
-										<input type="text" class="form-control" id="CodVerificacion" placeholder="Cod. Verificacion">
-									</div>
-									<div class="form-group col-12">
-										<label for="Nombre">Nombre:</label>
-										<input type="text" class="form-control" id="Nombre" placeholder="Nombres">
-									</div>
-									<div class="form-group col-12">
-										<label for="Paterno">Paterno:</label>
-										<input type="text" class="form-control" id="Paterno" placeholder="Apellido Paterno">
-									</div>
-									<div class="form-group col-12">
-										<label for="Materno">Materno:</label>
-										<input type="text" class="form-control" id="Materno" placeholder="Apellido Materno">
-									</div>
-								</form>
+								<table class="table table-striped">
+									<tbody id="tbody">
+									</tbody>
+								</table>
 							</div>
 						</div>
 					</div>
@@ -109,7 +93,7 @@
 			</div>
 			<footer class="footer text-center">
 				<div class="col">
-					<p><small>&copy; 2015 - 2017 JossMP - Derechos Reservados</small></p>
+					<p><small>&copy; 2015 - 2018 JossMP - Derechos Reservados</small></p>
 				</div>
 			</footer>
 		</div>
@@ -119,43 +103,44 @@
 		<script src="js/ajaxview.js"></script>
 		<script>
 			$(document).ready(function(){
-				$("#btn-submit").click(function(e){
+				$("#btn-submit-1, #btn-submit-2").click(function(e)
+				{
 					var $this = $(this);
+					$source = $this.data("source");
 					e.preventDefault();
-					//$this.button('loading');
 					$.ajaxblock();
 					$.ajax({
-						data: { "ndni" : $("#ndni").val() },
+						data: { "ndni" : $("#ndni").val(),"source" : $source },
 						type: "POST",
 						dataType: "json",
 						url: "consulta.php",
 					}).done(function( data, textStatus, jqXHR ){
-						if(data['success']!="false" && data['success']!=false)
+						if( data['success']!=false )
 						{
 							$("#json_code").text(JSON.stringify(data, null, '\t'));
 							if(typeof(data['result'])!='undefined')
 							{
-								$("#CodVerificacion").val( data["result"]["CodVerificacion"] );
-								$("#DNI").val( data["result"]["DNI"] );
-								$("#Nombre").val( data["result"]["Nombre"] );
-								$("#Materno").val( data["result"]["Materno"] );
-								$("#Paterno").val( data["result"]["Paterno"] );
+								$("#tbody").html("");
+								$.each(data['result'], function(i, v)
+								{
+									$("#tbody").append('<tr><td>'+i+'<\/td><td>'+v+'<\/td><\/tr>');
+								});
 							}
 							//$this.button('reset');
+							$("#error").hide();
+							$(".result").show();
 							$.ajaxunblock();
 						}
 						else
 						{
-							if(typeof(data['msg'])!='undefined')
+							if(typeof(data['message'])!='undefined')
 							{
-								alert( data['msg'] );
+								alert( data['message'] );
 							}
-							//$this.button('reset');
 							$.ajaxunblock();
 						}
 					}).fail(function( jqXHR, textStatus, errorThrown ){
 						alert( "Solicitud fallida:" + textStatus );
-						$this.button('reset');
 						$.ajaxunblock();
 					});
 				});
@@ -163,3 +148,4 @@
 		</script>
 	</body>
 </html>
+
--- src/autoload.php
+++ src/autoload.php
@@ -1,5 +1,7 @@
 <?php
+	require (__DIR__ . "/obj.php");
 	require (__DIR__ . "/curl.php");
-	require (__DIR__ . "/reniec.php");
-	require (__DIR__ . "/solver.php");
+	require (__DIR__ . "/essalud/essalud.php");
+	require (__DIR__ . "/mintra/mintra.php");
+	//require (__DIR__ . "/reniec/reniec.php");
 ?>
